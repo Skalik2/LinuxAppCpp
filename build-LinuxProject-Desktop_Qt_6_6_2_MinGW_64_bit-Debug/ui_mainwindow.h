ae502915d7decc68c731f19d0b5b862cb8726ecd
Skalik <106670239+Skalik2@users.noreply.github.com>
2024-03-03 21:37:01 +0100

    add: full screen app on startup

--- build-LinuxProject-Desktop_Qt_6_6_2_MinGW_64_bit-Debug/ui_mainwindow.h
+++ build-LinuxProject-Desktop_Qt_6_6_2_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -11,7 +11,10 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
 #include <QtWidgets/QMainWindow>
+#include <QtWidgets/QPushButton>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -20,6 +23,13 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralwidget;
+    QPushButton *pushButtonSubmit;
+    QLineEdit *lineEditLogin;
+    QLabel *labelLogin;
+    QLineEdit *lineEditPassword;
+    QLabel *labelPassword;
+    QPushButton *pushButton;
+    QPushButton *pushButton_2;
 
     void setupUi(QMainWindow *MainWindow)
     {
@@ -28,6 +38,27 @@ public:
         MainWindow->resize(800, 600);
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName("centralwidget");
+        pushButtonSubmit = new QPushButton(centralwidget);
+        pushButtonSubmit->setObjectName("pushButtonSubmit");
+        pushButtonSubmit->setGeometry(QRect(350, 440, 83, 29));
+        lineEditLogin = new QLineEdit(centralwidget);
+        lineEditLogin->setObjectName("lineEditLogin");
+        lineEditLogin->setGeometry(QRect(170, 180, 113, 28));
+        labelLogin = new QLabel(centralwidget);
+        labelLogin->setObjectName("labelLogin");
+        labelLogin->setGeometry(QRect(170, 150, 63, 20));
+        lineEditPassword = new QLineEdit(centralwidget);
+        lineEditPassword->setObjectName("lineEditPassword");
+        lineEditPassword->setGeometry(QRect(170, 260, 113, 28));
+        labelPassword = new QLabel(centralwidget);
+        labelPassword->setObjectName("labelPassword");
+        labelPassword->setGeometry(QRect(170, 230, 63, 20));
+        pushButton = new QPushButton(centralwidget);
+        pushButton->setObjectName("pushButton");
+        pushButton->setGeometry(QRect(260, 440, 83, 29));
+        pushButton_2 = new QPushButton(centralwidget);
+        pushButton_2->setObjectName("pushButton_2");
+        pushButton_2->setGeometry(QRect(310, 400, 83, 29));
         MainWindow->setCentralWidget(centralwidget);
 
         retranslateUi(MainWindow);
@@ -38,6 +69,11 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        pushButtonSubmit->setText(QCoreApplication::translate("MainWindow", "Login", nullptr));
+        labelLogin->setText(QCoreApplication::translate("MainWindow", "Login:", nullptr));
+        labelPassword->setText(QCoreApplication::translate("MainWindow", "Password:", nullptr));
+        pushButton->setText(QCoreApplication::translate("MainWindow", "Clear", nullptr));
+        pushButton_2->setText(QCoreApplication::translate("MainWindow", "Clear", nullptr));
     } // retranslateUi
 
 };
